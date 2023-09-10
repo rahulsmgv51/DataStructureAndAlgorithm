bbd8c3f69e85a3f4977c1a8c10ac996970c3b8cb
rahulsmgv51 <rahulsmgv@gmail.com>
2023-09-10 20:54:48 +0530

    Sorting

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -13,7 +13,6 @@ public class BubbleSort {
             }
         }
     }
-
     // used for swapping ith and jth elements of array
     public static void swap(int[] arr, int i, int j) {
         System.out.println("Swapping " + arr[i] + " and " + arr[j]);
@@ -31,13 +30,11 @@ public class BubbleSort {
             return false;
         }
     }
-
     public static void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
-
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
