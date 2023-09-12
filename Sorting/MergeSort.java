b4ff2dfc777efa67d7f12cd5ec84fc9e89361302
rahulsmgv51 <rahulsmgv@gmail.com>
2023-09-12 08:04:32 +0530

    Merge Sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,8 +1,65 @@
 package Sorting;
+import java.util.*;
+
 public class MergeSort {
-    public static void main(String[] args) {
-        int arr[] = {8,4,3,2,6,7,9,0,1,5};
-        int len = arr.length;
+    public static int[] mergeSort(int[] arr, int lo, int hi) {
+        if(lo == hi){
+            int[] base = new int[1];
+            base[0] = arr[lo];
+            return base;
+        }
+        int mid = (lo + hi)/2;
+        int[] fsa = mergeSort(arr, lo, mid);
+        int[] ssa = mergeSort(arr, mid+1, hi);
+        int[] sortedArr = mergeTwoSortedArrays(fsa, ssa);
+        return sortedArr;
+    }
+
+    public static int[] mergeTwoSortedArrays(int[] a, int[] b) {
         int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] arr = new int[a.length + b.length];
+        while (i < a.length && j < b.length) {
+            System.out.println("Compare " + a[i] + " and " + b[j] + " i " + i + " j " + j + " k " + k);
+            if (a[i] < b[j]) {
+                arr[k] = a[i];
+                i++;
+            } else {
+                arr[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < a.length) {
+            System.out.println("Compare " + a[i] + " and " + b[j - 1] + " i " + i + " j " + j + " k " + k);
+            arr[k] = a[i];
+            i++;
+            k++;
+        }
+        while (j < b.length) {
+            System.out.println("Compare " + a[i - 1] + " and " + b[j] + " i " + i + " j " + j + " k " + k);
+            arr[k] = b[j];
+            j++;
+            k++;
+        }
+        return arr;
+    }
+
+    public static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        int[] sa = mergeSort(arr, 0, arr.length - 1);
+        print(sa);
     }
 }
--- Sorting/Readme.md
+++ Sorting/Readme.md
@@ -4,4 +4,5 @@
 
 ### Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
 
-### 
+### Merge sort is defined as a sorting algorithm that works by dividing an array into smaller subarrays, sorting each subarray, and then merging the sorted subarrays back together to form the final sorted array.
+
