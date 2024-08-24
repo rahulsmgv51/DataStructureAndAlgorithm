45ca6a8f45636f859f1aebbebfb111e6673d082a
rahulsmgv51 <rahulsmgv@gmail.com>
2024-08-24 07:58:37 +0530

    Abstraction

---
+++ Opps/Abstraction/Readme.md
@@ -0,0 +1,13 @@
+### Java Abstract Class
+
+1. The abstract class in java cannot be instantiated(we can not create objects of abstract classes). We use the abstract keyword to declare an abstract class.
+
+2. An abstract class can have both the regular methods and abstrct methods.
+3. A method that doesn't have its body is known as an abstract method.
+4. Though abstract classes cannot be instantiated, we can create subclasses from it. We can then access members of the abstract class using the object of the subclass.
+5. If the abstract class includes any abstract method, then all the child classes inherited from the abstract superclass must provide the implementation of the abstract method.
+
+
+### Abstraction
+1. Abstraction is an important concept of object-oriented programming that allows us to hide unnecessary details and only show the needed infromation.
+this allows us to manage complexity by omitting or hiding details with a simpler higher-level idea.
---
+++ Opps/Abstraction/class_and_method_2nd_example/LearnAbstract.java
@@ -0,0 +1,52 @@
+package Opps.Abstraction.class_and_method_2nd_example;
+
+public class LearnAbstract {
+    public static void main(String[] args) {
+        // Cannot instantiate the type Animal
+        // Animal a1 = new Animal();
+        Dog dog = new Dog();
+        dog.eat();
+        dog.walk();
+        dog.legs(4);
+        dog.sleep();
+    }
+}
+
+abstract class Animal {
+    abstract void eat();
+
+    abstract void walk();
+
+    abstract int legs(int legs);
+
+    void sleep(){
+        System.out.println("Cow sleep");
+    }
+}
+
+class Dog extends Animal {
+
+    /*
+     * Indicates that a method declaration is intended to override a method
+     * declaration in a supertype. If a method is annotated with this annotation
+     * type compilers are required to generate an error message unless at least one
+     * of the following conditions hold:
+     * The method does override or implement a method declared in a supertype.
+     * The method has a signature that is override-equivalent to that of any public
+     * method declared in Object.
+     */
+    @Override
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    @Override
+    void walk() {
+        System.out.println("Dog is runnig");
+    }
+
+    @Override
+    int legs(int legs) {
+        return legs;
+    }
+}
