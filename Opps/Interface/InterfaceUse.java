a899f28d11f7d8c9fe4528a7247e9f8795f2b80b
rahulsmgv51 <rahulsmgv@gmail.com>
2024-08-25 23:25:45 +0530

    interface

--- Opps/Interface/InterfaceUse.java
+++ Opps/Interface/InterfaceUse.java
@@ -1,5 +1,48 @@
 package Opps.Interface;
 
 public class InterfaceUse {
-    
-} 
+    // Cannot instantiate the type Animal
+    // Animal animal = new Animal();
+
+    public static void main(String[] args) {
+        Cow cow = new Cow();
+
+        System.out.println(Human.LEGS);
+        
+        // The final field Human.LEGS cannot be assigned
+        // Human.LEGS =4;
+    }
+}
+
+interface Animal {
+    // In Interface methods are by default public and abstract. and also we can
+    // remove abstract keyword from below method.
+    public abstract void eats();
+
+    void sleep();
+}
+
+interface Human {
+    // all fields are implicitly public static final.
+    // public static final int LEGS = 2; this and below statement are same we cannot change.
+    int LEGS = 2;
+    void run();
+}
+
+class Cow implements Animal, Human {
+
+    @Override
+    public void eats() {
+        System.out.println("Cow is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Cow is sleeping");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Cow is running");
+    }
+}
--- Opps/Interface/Readme.md
+++ Opps/Interface/Readme.md
@@ -3,3 +3,11 @@
 2. We use the interface keyword to create an interface in java.
 3. Like abstract classes, we cannot create objects of interface.
 4. To use an interface, other classes must implement it. We use the implements keyword to implement an interface.
+5. We cannot extends multiple abtract class.
+6. In Interface methods are by default abstract.
+
+
+#### Advatages of java Interfaces
+1. Similar to abstract classes, interfaces help us to acheive abstraction in java.
+2. Interfaces are also used to acheive multiple inheritance in java.
+3. All the methods inside an interface are implicitly public and all fields are implicitly public static final.
