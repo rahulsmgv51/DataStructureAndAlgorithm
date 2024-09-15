d89ef04747e7404f056d203d91367a7f517bff87
rahulsmgv51 <rahulsmgv@gmail.com>
2024-09-15 13:35:15 +0530

    sumOfNNumberBasicRecursion

--- Recursion/RecuresionMore/BasicRecursion.java
+++ Recursion/RecuresionMore/BasicRecursion.java
@@ -3,13 +3,13 @@ package RecuresionMore;
 public class BasicRecursion {
     public static void main(String[] args) {
         // fun(5);
-        System.out.println(sumOfNNumbwr(5));
+        System.out.println(sumOfNNumber(5));
     }
-    static int sumOfNNumbwr(int n){
+    static int sumOfNNumber(int n){
         if(n==1){
             return 1;
         }
-        int sum = sumOfNNumbwr(n-1)+n;
+        int sum = sumOfNNumber(n-1)+n;
         return sum;
     }
     // static void fun(int n) {
